e1a4d920554325e29b102f64c089daa1401f22dd
ahgr3y <leehh97@gmail.com>
2024-08-05 15:29:58 +0800

    Add boilerplate

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/AhGr3y/my-grocery-list/internal/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+type appConfig struct {
+	DB *database.Queries
+}
+
 func main() {
-	// Load environment variables in .env
-	err := godotenv.Load(".env")
+	// Load environment variables in .env0
+	godotenv.Load()
+
+	// Get database url from .env
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
+
+	// Open a connection to the database
+	db, err := sql.Open("postgres", dbUrl)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	// Initialize appConfig
+	dbQueries := database.New(db)
+	cfg := appConfig{
+		DB: dbQueries,
 	}
 
 	fmt.Println("Hello world!")
