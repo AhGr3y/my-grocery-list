58cede81d9d012d72c4ce80e62f1051ad28f4da5
ahgr3y <leehh97@gmail.com>
2024-08-08 15:19:11 +0800

    Updated exit command

--- command_exit.go
+++ command_exit.go
@@ -1,8 +1,14 @@
 package main
 
-import "os"
+import (
+	"errors"
+	"os"
+)
 
 func commandExit(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("'exit' does not require additional arguments. Use 'exit' instead")
+	}
 	os.Exit(1)
 	return nil
 }
