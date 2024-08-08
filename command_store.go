c50ce2987bfa9f8b70fd4f4f0d2d2540ae3efc76
ahgr3y <leehh97@gmail.com>
2024-08-08 16:43:48 +0800

    Updated store command

--- command_store.go
+++ command_store.go
@@ -12,6 +12,19 @@ func checkBrand(brand string) bool {
 	return false
 }
 
+func promptUserForYesNo() (bool, error) {
+	reader := bufio.NewScanner(os.Stdin)
+	fmt.Print("Do you want to continue? [Y/n] ")
+	reader.Scan()
+	userInput := reader.Text()
+	proceed, err := processYesNo(userInput)
+	if err != nil {
+		return false, err
+	}
+
+	return proceed, nil
+}
+
 func commandStore(cfg *config, args ...string) error {
 	// Store commands takes at least 3 arguments:
 	// store <item_name> <brand_name> [quantity]
@@ -32,19 +45,30 @@ func commandStore(cfg *config, args ...string) error {
 		item_quantity = 1
 	}
 
+	// TODO: GET INVENTORY QUANTITY
+
 	// Get user confirmation to create new brand
+	item_name := args[1]
 	item_brand := args[2]
 	brandExist := checkBrand(item_brand)
 	if !brandExist {
-		reader := bufio.NewScanner(os.Stdin)
-		fmt.Printf("Brand '%s' does not exist. Do you want to continue? [Y/n] ", item_brand)
-		reader.Scan()
-		userInput := reader.Text()
-		fmt.Printf("You have entered: %s\n", userInput)
+		// Loop until user input valid response
+		for {
+			fmt.Printf("Brand '%s' does not exist. ", item_brand)
+			proceed, err := promptUserForYesNo()
+			if err != nil {
+				continue
+			}
+			if !proceed {
+				fmt.Printf("%s %s not stored\n", item_brand, item_name)
+				return nil
+			}
+			break
+		}
 	}
+	// Add item to inventory
 
 	// Output item quantity to user
-	item_name := args[1]
 	fmt.Printf("Number of %s %s on hand: %d\n", item_brand, item_name, item_quantity)
 
 	return nil
